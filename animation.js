58c5d7c203af22b2164a2024d9b2ad9529493ec1
AxelJt <jtaxel92@gmail.com>
2020-07-18 00:58:25 +0200

    Update animation.js

--- animation.js
+++ animation.js
@@ -2,7 +2,7 @@
 var textWrapper = document.querySelector('.ml11 .letters');
 textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
 
-anime.timeline({loop: true})
+anime.timeline({loop: false})
   .add({
     targets: '.ml11 .line',
     scaleY: [0,1],
