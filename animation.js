433ac058e11d6a4c283698d31a79c615865daf25
AxelJt <jtaxel92@gmail.com>
2020-07-19 00:47:38 +0200

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,6 +1,7 @@
 // Wrap every letter in a span
 var textWrapper = document.querySelector('.ml11 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/([^.\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+textWrapper.innerHTML = textWrapper.textContent.replace(".", "<span class='letter'>.</span>");
 
 anime.timeline({loop: true})
   .add({
